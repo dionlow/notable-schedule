56b568b7ff8e3814179f5dd16ecc02732b5b1450
Dion Low <dionlow17@gmail.com>
2019-03-14 16:03:51 -0700

    prop firstName fix

--- src/components/PhysicianTitle/PhyscianTitle.js
+++ src/components/PhysicianTitle/PhyscianTitle.js
@@ -1,7 +1,7 @@
 import React from 'react'
 const physicianTitle = (props) => (
     <div>
-        <h1>Dr. {props.physican.firstName} {props.physican.nameLast}</h1>
+        <h1>Dr. {props.physican.nameFirst} {props.physican.nameLast}</h1>
         <p>{props.physican.email}</p>
     </div>
 )
